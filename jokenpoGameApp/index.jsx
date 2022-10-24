583fd66a7a8a2519304f746697e9d3a23c48907e
belluzzojr <98968823+belluzzojr@users.noreply.github.com>
2022-10-24 17:55:17 -0300

    Delete jokenpoGameApp directory

--- jokenpoGameApp/index.jsx
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import HomeController from './src/Screens/Home/HomeController';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => HomeController);
